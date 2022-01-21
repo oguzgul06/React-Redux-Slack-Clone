2568e6f059dc0f92e0beac2b0e034da0f9dc03a0
Oğuz GÜL <ogzgl06@gmail.com>
2022-01-21 11:21:33 +0300

    Create Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's Build Slack Clone</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar
+          className="header__avatar"
+          /* alt={user?.displayname}
+          src={user?.photo} */
+        />
+
+        <AccessTimeIcon />
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+        <input placeholder="Search Programmer"/>
+      </div>
+      <div className="header__right">
+        <HelpOutlineOutlinedIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
