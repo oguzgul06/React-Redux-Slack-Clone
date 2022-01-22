469d84b177988d3287130c9b0b0cb35b22aa8010
Oğuz GÜL <ogzgl06@gmail.com>
2022-01-22 20:04:28 +0300

    Create login component and setup google authentication with firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Chat from "./components/Chat";
+import Login from "./components/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Routes>
-            <Route path="/room/:roomId" element={<Chat />} />
-            <Route path="/" element={<h1>Welcome</h1>} />
-          </Routes>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Routes>
+                <Route path="/room/:roomId" element={<Chat />} />
+                <Route path="/" element={<h1>Welcome</h1>} />
+              </Routes>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "../firebase";
+
+function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfaeUSn6az3w_sR784pPCdxQGG7R198zd6Tw&usqp=CAU"
+          alt="slack__logo"
+        />
+        <h1>Sign in to Brothers Programmer</h1>
+        <p>brothersprogrammer.slack.com</p>
+        <Button onClick={signIn}>Sign In with Google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
